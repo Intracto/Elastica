a252431e786dcd89291228645726af34ca6fc6f8
Jasper Lammens <jasper.lammens@intracto.com>
2017-04-10 15:53:05 +0200

    Add support for named queries

--- lib/Elastica/Query/AbstractQuery.php
+++ lib/Elastica/Query/AbstractQuery.php
@@ -10,4 +10,51 @@ use Elastica\Param;
  */
 abstract class AbstractQuery extends Param
 {
+    /**
+     * The name of the query.
+     *
+     * @var string
+     */
+    protected $_name;
+
+    /**
+     * Get the name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * Set the name.
+     *
+     * @param string $_name
+     *   The name to be set.
+     *
+     * @return $this
+     */
+    public function setName($_name)
+    {
+        $this->_name = $_name;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toArray()
+    {
+        $data = parent::toArray();
+        if ($this->_name) {
+            $baseName = $this->_getBaseName();
+            if (is_array($data[$baseName])) {
+                $data[$baseName]['_name'] = $this->_name;
+            }
+        }
+
+        return $data;
+    }
 }
